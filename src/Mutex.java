3970257f86ac4d3e7efae1797a52222350487590
Zeyadptd <zeyad.habashy02@gmail.com>
2023-05-19 22:33:29 +0300

    removed some wrong stuff

--- src/Mutex.java
+++ src/Mutex.java
@@ -9,32 +9,5 @@ public class Mutex {
 
 
 
-    // need to change this to not use wait() and notify() and instead use semWait() and semSignal()
 
-    // semWait is the same as lock
-    public synchronized void lock(int processID) { // processID is the ID of the process that is trying to lock the mutex
-        while (locked) { // while the mutex is locked,
-            try {
-                wait(); // wait until the mutex is unlocked
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        ownerID = processID; // else, give the mutex to the process that is trying to lock it
-        locked = true; // and set the mutex to be locked
-    }
-
-
-    // semSignal is the same as unlock
-    public synchronized void unlock(int processID) { // processID is the ID of the process that is trying to unlock the mutex
-        if (ownerID == -1) { // if the mutex is not locked,
-            return; // then return
-        }
-        if (ownerID != processID) { // if the process that is trying to unlock the mutex is not the owner of the mutex,
-            return; // then return
-        }
-        ownerID = -1; // else, unlock the mutex
-        locked = false; // and set the mutex to be available
-        notify(); // then notify all waiting processes
-    }
 }
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -8,9 +8,9 @@ public class Scheduler {
     private Queue<Integer> blockedQueue;
     private MemoryWord[] memory;
     private int runningProcessID;
-    private Mutex inputMutex;
-    private Mutex outputMutex;
-    private Mutex diskMutex; // accessing a file on disk
+    private static Mutex inputMutex;
+    private static Mutex outputMutex;
+    private static Mutex fileMutex; // accessing a file on disk
     private int timeSlice; // represents the time slice for the round robin algorithm
 
     // need a variable for current instruction
