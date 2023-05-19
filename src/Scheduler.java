b5f4404f7061b804493cc884c855becbbf9cfe58
Zeyadptd <zeyad.habashy02@gmail.com>
2023-05-19 22:03:10 +0300

    Added some variables to mutex and Scheduler, could remove or change idk

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,4 +1,17 @@
 package src;
 
+
+import java.util.Queue;
+
 public class Scheduler {
+    private Queue<Integer> readyQueue;
+    private Queue<Integer> blockedQueue;
+    private MemoryWord[] memory;
+    private int runningProcessID;
+    private Mutex inputMutex;
+    private Mutex outputMutex;
+    private Mutex diskMutex; // accessing a file on disk
+    private int timeSlice; // represents the time slice for the round robin algorithm
+
+    // need a variable for current instruction
 }
