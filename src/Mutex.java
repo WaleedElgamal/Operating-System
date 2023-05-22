0f964e810f5004c730a5c7c5679300fcda9ecc83
Zeyadptd <zeyad.habashy02@gmail.com>
2023-05-22 17:28:02 +0300

    replaced programs with proper ones

--- src/Mutex.java
+++ src/Mutex.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Mutex {
@@ -7,7 +8,53 @@ public class Mutex {
     private int ownerID = -1; // -1 means no owner
     private Queue<Integer> blockedQueue;
 
+    public Mutex() {
+        this.blockedQueue = new LinkedList<>();
+    }
 
+    public void semWait(int pid) {
+        if (locked) {
+            blockedQueue.add(pid);
+        } else {
+            locked = true;
+            ownerID = pid;
+        }
+    }
 
+    public void semSignal(int pid) {
+        if(ownerID != pid) {
+            return;
+        }
+        release();
+    }
 
+    private void release(){
+        if (blockedQueue.isEmpty()) {
+            locked = false;
+            ownerID = -1;
+        } else {
+            ownerID = blockedQueue.remove();
+        }
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+    public void setOwnerID(int ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public Queue<Integer> getBlockedQueue() {
+        return blockedQueue;
+    }
+    public void setBlockedQueue(Queue<Integer> blockedQueue) {
+        this.blockedQueue = blockedQueue;
+    }
 }
