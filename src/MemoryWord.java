2d4b32bbfb2ae7454dbe8b6f1efe4b59dcb0ba63
shrouqq <shorokabdulraof@icloud.com>
2023-05-23 14:45:11 +0300

    SystemCalls half done, missing memory class

--- src/MemoryWord.java
+++ src/MemoryWord.java
@@ -2,11 +2,27 @@ package src;
 
 public class MemoryWord {
     private String variableName;
+
+    public MemoryWord(String variableName, Object value) {
+        this.variableName = variableName;
+        this.value = value;
+    }
+
     private Object value;
 
-    public MemoryWord(String variableName, Object value)
-    {
-        this.variableName=variableName;
-        this.value=value;
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
     }
 }
