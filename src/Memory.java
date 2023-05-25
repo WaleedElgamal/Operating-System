0a56bcf4212fe0972e1bb6cfaf826fd24078079d
NinzXtrmz <ninakhaled2002@gmail.com>
2023-05-25 20:30:48 +0300

    just... added a bracket... made the P small...

--- src/Memory.java
+++ src/Memory.java
@@ -9,8 +9,8 @@ public class Memory {
     boolean p2;
 
     public Memory() {
-        p1=false;
-        p2=false;
+        p1 = false;
+        p2 = false;
     }
 
     public MemoryWord[] getMemory() {
@@ -24,9 +24,9 @@ public class Memory {
 
     public int hasSpace() //returns -1 if no empty spaces
     {
-        if(Memory[15]==null )//&& taken[26]==false) //first slot of process is empty + maybe 26 wont be used
+        if (Memory[15] == null)//&& taken[26]==false) //first slot of process is empty + maybe 26 wont be used
             return 15;
-        else if(Memory[27]==null)// && taken[39]==false) //second slot of process is empty + maybe 39 wont be used
+        else if (Memory[27] == null)// && taken[39]==false) //second slot of process is empty + maybe 39 wont be used
             return 27;
         else
             return -1; //both slots are occupied
@@ -39,61 +39,63 @@ public class Memory {
         // else // need to check if there is space outside and checking which process to pre empt
 
     }
+
     public void insertIntoMemory(int position, Process p) //position read from pcv attribute?
     { //TODO handle program attribute in process for this logic to work
 
-        String program=p.getProgram(); //gets the string file of the instructions
-        String[] programSplit= program.split("\n");
+        String program = p.getProgram(); //gets the string file of the instructions
+        String[] programSplit = program.split("\n");
 
-        for(int i=position; i<program.length();i++)
-        {
+        for (int i = position; i < program.length(); i++) {
 
-            Memory[i]= new MemoryWord("",programSplit);
+            Memory[i] = new MemoryWord("", programSplit);
 
         }
 
-        switch(P.getProcessOrder) ////Fix memory bounds in PCB
+        switch (p.getProcessOrder) ////Fix memory bounds in PCB
         {
             case 1:
-                Memory[3]=p.getMemoryStart();
-                Memory[4]=p.getMemoryStart()+12;
+                Memory[3] = p.getMemoryStart();
+                Memory[4] = p.getMemoryStart() + 12;
                 break;
             case 2:
-                Memory[8]=p.getMemoryStart();
-                Memory[9]=p.getMemoryStart()+12;
+                Memory[8] = p.getMemoryStart();
+                Memory[9] = p.getMemoryStart() + 12;
                 break;
             case 3:
-                Memory[13]=p.getMemoryStart();
-                Memory[14]=p.getMemoryStart()+12;
+                Memory[13] = p.getMemoryStart();
+                Memory[14] = p.getMemoryStart() + 12;
                 break;
 
 
         }
+    }
 
-    public void removeFromMemory(Process p) //deletes process from memory by nullifying its segment
-    {
+        public void removeFromMemory (Process p) //deletes process from memory by nullifying its segment
+        {
             int position = p.getP(); //returns P1 or P2
-            int begin=P.getMemoryStart(); //reutrns memory bound 1
-            int end=begin+ 12; //
-            for(int i=begin;i<=end;i++) //nullifies position
+            int begin = p.getMemoryStart(); //reutrns memory bound 1
+            int end = begin + 12; //
+            for (int i = begin; i <= end; i++) //nullifies position
             {
-                Memory[i]=null;
+                Memory[i] = null;
             }
-            switch(P.getProcessOrder) //nullify memory bounds in PCB
+            switch (p.getProcessOrder) //nullify memory bounds in PCB
             {
                 case 1:
-                    Memory[3]=null;
-                    Memory[4]=null;
+                    Memory[3] = null;
+                    Memory[4] = null;
                     break;
                 case 2:
-                    Memory[8]=null;
-                    Memory[9]=null;
+                    Memory[8] = null;
+                    Memory[9] = null;
                     break;
                 case 3:
-                    Memory[13]=null;
-                    Memory[14]=null;
+                    Memory[13] = null;
+                    Memory[14] = null;
                     break;
 
             }
+        }
     }
-}
+
